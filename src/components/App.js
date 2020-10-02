b57d40b3fc78d744a1a9e69fb1f1f7ac0e7bb41d
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-10-01 18:21:35 -0700

    add less.js package

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RecipeControl from './RecipeControl';
+
+function App() {
+  return (
+    <React.Fragment>
+      <RecipeControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
